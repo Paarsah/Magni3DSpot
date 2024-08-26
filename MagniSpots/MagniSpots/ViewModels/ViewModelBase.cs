54b1d7bbcf8f2ba58dd71f6f36ab7f8613451ac3
Paarsah Soroury <paarsah.soroury@gmail.com>
2024-08-25 22:56:40 -0700

    Initiation

---
+++ MagniSpots/MagniSpots/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MagniSpots.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ MagniSpots/MagniSpots/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MagniSpots.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
